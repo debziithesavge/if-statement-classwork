6f32db8bf28c040d28bb85a511baa0d9c5c16759
DEBZ <debelezefavour@gmail.com>
2026-08-18 11:52:36 +0100

    latest change

---
+++ index.js
@@ -0,0 +1,13 @@
+let Password = "The Master"
+let Role = "The Admin"
+
+if (Role === "The Admin"){
+    if(Password === "The Master"){
+        console.log("Welcome Master");
+    }else{
+        console.log("I don't know you!!!")
+    }
+}else{
+    console.log("This is not an Admin");
+
+}
